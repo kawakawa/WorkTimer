1f78b34b8dcffa112711d40fd0b7bdc8e1698762
TakuyaKawabe <rapidarrow2000@gmail.com>
2018-07-25 15:48:54 +0900

    ソリューション作成＆アプリひな形作成

---
+++ WorkTImer/WorkTImer/ViewModel/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace WorkTImer.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+
+    }
+}
---
+++ WorkTImer/WorkTImer/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkTImer.View;
+
+namespace WorkTImer.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private readonly View.MainWindow _window;
+
+        public MainWindowViewModel()
+        {
+            _window =new MainWindow(this);
+        }
+
+        public void Show()
+        {
+            _window.Show();
+        }
+    }
+}

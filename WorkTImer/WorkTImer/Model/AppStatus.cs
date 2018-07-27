70a706d414210d2aa5b13617feb1b7c2bfd28bf3
TakuyaKawabe <rapidarrow2000@gmail.com>
2018-07-27 17:16:41 +0900

    モードクラス追加

---
+++ WorkTImer/WorkTImer/Model/AppStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkTImer.Model
+{
+    public class AppStatus
+    {
+        public Mode NowMode { get; set; }
+    }
+}
